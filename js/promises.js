da204ae31ed3a3bc8fe38cd06be57d226ec7a635
Knoah Cotto <cotto.knoah@gmail.com>
2020-05-26 13:23:04 -0500

    promises exercise complete

--- js/promises.js
+++ js/promises.js
@@ -1,44 +1,62 @@
 "use strict";
 
-var url = 'https://api.github.com/users';
+// 1. Create js/promises.js and link it in an html file
+// 2. Generate a GitHub API personal access token (add to keys.js)
+// 3. Create a function that accepts anyone's GitHub username and returns a promise that resolves returning just the date of the last commit that user made.
 
-var githubAPI = fetch(url, {
-    headers: {
-        'Authorization': `token ${GITHUB_API_KEY}`
-    }
-});
+    // var url = 'https://api.github.com/users';
+    // var githubAPI = fetch(url, {
+    //     headers: {
+    //         'Authorization': `token ${GITHUB_API_KEY}`
+    //     }
+    // });
 
+// console.log(githubAPI)
 
-fetch('https://api.github.com/users')
-    .then(response => response.json())
-    .then(users => {
-        users.forEach( userObj => {
-            // do something with each username
-            console.log(userObj.login);
-        })
-    })
-    .catch(error => console.error(error));
+// ======================================#1
+// Create a function that accepts a GitHub username, and returns a promise that
+// resolves returning just the date of the last
+// commit that user made.
+// Reference the github api documentation to achieve this.
 
+//make a function that takes in GitHube username
+//returns a promise
+//resolves to date of the most recent commit
 
-
-function makeRequest() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (Math.random() > 0.1) {
-                resolve('Here is your data: ...');
-            } else {
-                reject('Network Connection Error!');
-            }
-        }, 1500);
-    });
+function getLastCommitDate(username) {
+    return fetch(`https://api.github.com/users/${username}/events`, {
+        headers: {'Authorization': `token ${GITHUB_API_KEY}`}
+    }).then(response => response.json())
+        //turning it into an promise object above
+        .then(events => {
+           return events.filter(event => event.type ==="PushEvent")[0].created_at;
+        });
 }
 
-const request = makeRequest();
-console.log(request); // pending promise
-request.then(message => console.log('Promise resolved!', message));
-// if resolved, will log "Promise resolved!" and "Here is your data: ..."
-request.catch(message => console.log('Promise rejected!', message));
-// if rejected, will log "Promise rejected!" and "Network Connection Error!"
+getLastCommitDate('cottoknoah').then(date => {
+    console.log(new Date(date).toDateString());
+});
+
+// ==================================== #2
+// Write a function named wait that accepts a number as a parameter, and
+// returns a promise that resolves after the passed number of milliseconds.
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
+function wait(seconds){
+    seconds = seconds * 1000;
+    return new Promise(resolve => {
+        setTimeout(() => {
+            if(seconds > 1000){
+                console.log("You'll see this after " + seconds/1000 + " seconds.");
+            } else {
+                console.log("You'll see this after " + seconds/1000 + " second.");
+            }
+        },seconds)
+    })
+}
 
+wait(1);
+wait(2);
+wait(3);
